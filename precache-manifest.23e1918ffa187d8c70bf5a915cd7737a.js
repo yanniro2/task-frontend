56b7484263acf1b62192bdafa18bbce9f962e4b9
yanniro2 <k.niroyan@gmail.com>
2022-07-06 11:46:51 +0530

    Updates

---
+++ precache-manifest.23e1918ffa187d8c70bf5a915cd7737a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0b5da90f92f9972d606",
+    "url": "/task-frontend/static/css/main.b3a97f8a.chunk.css"
+  },
+  {
+    "revision": "d0b5da90f92f9972d606",
+    "url": "/task-frontend/static/js/main.d0b5da90.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/task-frontend/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "72196cd369f8cef24ae2",
+    "url": "/task-frontend/static/js/2.72196cd3.chunk.js"
+  },
+  {
+    "revision": "1196f235bbd74c69dc10",
+    "url": "/task-frontend/static/js/runtime~main.1196f235.js"
+  },
+  {
+    "revision": "91de43f5c8d3d6fb08762793fb588988",
+    "url": "/task-frontend/static/media/Mask Group 1.91de43f5.png"
+  },
+  {
+    "revision": "762b25e32ac4004cf5dc8270d3db6633",
+    "url": "/task-frontend/index.html"
+  }
+];
